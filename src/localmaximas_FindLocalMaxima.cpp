02ac0b4c5bd52d6dca373337964bbc76ee8749e5
frank2165 <marc.fletcher1990@gmail.com>
2018-12-20 01:24:08 +1000

    Update
    Runtime errors persist, first iteration of parallel for loop seems to run fine, can't get any diagnostics on other threads. Still works fine in single threaded mode.

--- src/localmaximas_FindLocalMaxima.cpp
+++ src/localmaximas_FindLocalMaxima.cpp
@@ -3,7 +3,7 @@ This function processes an R vector of file handles into a list of coordinates o
 local maxima within each file. OpenMP is used in an attempt to minimise the program runtime.
 */
 
-
+#include <iostream>
 #include <omp.h>
 #include "localmaximas.h"
 
@@ -30,11 +30,15 @@ Rcpp::List FindLocalMaxima(Rcpp::List handles, double radius, int numCores){
 	std::vector<arma::Mat<double>>::iterator it = maxima.begin();
 	// Rcpp::List maximaList;
 
+	Rprintf("Files in dataset: %i\n", numFiles);
+
 	while(it != maxima.end()) {
 		current_pos = std::distance(maxima.begin(), it); // Does this add any actual safety or could you just update current pos at the end of the while loop?
 		if ((current_pos + numCores) > numFiles) {
 			numCores = numFiles - current_pos;
 		}
+
+		Rprintf( "Reading datasets: %i - %i\n", current_pos, current_pos + numCores-1);
 
 		for (int j = 0; j < numCores; j++) {
 			dataList[j] = ReadDataset(handles[current_pos + j]);
@@ -48,7 +52,9 @@ Rcpp::List FindLocalMaxima(Rcpp::List handles, double radius, int numCores){
 #pragma omp for private (data)
 			for (int thread = 0; thread < numCores; thread++) {
 
-				data = dataList[thread];
+				int threadID = omp_get_thread_num();
+
+				data = dataList[threadID];
 
 				// Make the coordinates
 				arma::Mat<double> coords = SetCoordinates(data);
@@ -69,7 +75,13 @@ Rcpp::List FindLocalMaxima(Rcpp::List handles, double radius, int numCores){
 				data.z = data.z.rows(idxMaxima);
 				coords.insert_cols(coords.n_cols, data.z);
 
-				(it+thread)* = coords; // Something about l & r expressions...
+#pragma omp critical
+				{
+					std::cout << "Thread = " << threadID << " writing to maximas" << std::endl;
+					std::cout << "Writing to maximas[" << std::distance(maxima.begin(), it) << "]" << std::endl;
+					*(it + threadID) = coords;
+					std::cout << "Write complete" << std::endl;
+				}
 			}
 
 		}
@@ -78,10 +90,15 @@ Rcpp::List FindLocalMaxima(Rcpp::List handles, double radius, int numCores){
 	}
 	
 
+	Rprintf("Converting std::vector to Rcpp::List\n");
+
 	// Explicitly convert the std::vector to Rcpp::List
 	// maximaList = Rcpp::as<Rcpp::List>(Rcpp::wrap(maxima));
-	Rcpp::List maximaList(maxima.begin(), maxima.end());
+	Rcpp::List maximaList(maxima.size());
+	for (unsigned int i = 0; i < maxima.size(); i++) {
+		maximaList[i] = Rcpp::as<Rcpp::NumericMatrix>(Rcpp::wrap(maxima[i]));
+	}
 
-	return maxima;
+	return maximaList;
 }
 
--- tests/test_script.R
+++ tests/test_script.R
@@ -7,6 +7,7 @@ tifs <- dir("tests", pattern = "\\.tif$", full.names = TRUE, recursive = TRUE)
 # Attach Packages
 library(localmaximas)
 
+system.time(local_maxima_search(tifs, radius, 4))
 system.time(local_maxima_search(tifs, radius, 1))
 
 gc()
